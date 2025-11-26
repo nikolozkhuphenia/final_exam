55456f507c8fe7855581501f0724fefb98b79137
Nikoloz Khuphenia <nikolozkhuphenia@icloud.com>
2025-11-26 23:09:29 +0400

    bind port

--- app.py
+++ app.py
@@ -225,4 +225,5 @@ def api_example():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's port, default to 5000 locally
+    app.run(host='0.0.0.0', port=port)
